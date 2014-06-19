1506faa8e189b097f2dcfd24fa2bd1f9f9dac1ea
Nikolay Yurin <nyurin@griddynamics.com>
2014-06-19 13:11:47 +0400

    Test page was created to be accessed by Se Nodes
    Now when tests start, they start embedded server as well to show a test page
    Se nodes are going to access. In this way we can test complex things like
    native events because we can put anything we want on this page.
    
    As for now tests check:
     - whether an element can be found on the page
     - Native Events (drag'n'drop) - fixes #1
     - Executing JavaSript by WebDriver
     - access to external resources from the Internet like JS, Images, etc.

--- selenium-infrastructure-tests/src/test/groovy/com/griddynamics/cd/selenium/SeNodesSmokeTest.groovy
+++ selenium-infrastructure-tests/src/test/groovy/com/griddynamics/cd/selenium/SeNodesSmokeTest.groovy
@@ -4,10 +4,13 @@
  */
 package com.griddynamics.cd.selenium
 
+import org.eclipse.jetty.server.Server
+import org.eclipse.jetty.server.ServerConnector
 import org.openqa.selenium.By
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.remote.DesiredCapabilities
 import org.openqa.selenium.remote.RemoteWebDriver
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.test.context.ContextConfiguration
 import spock.lang.Shared
@@ -16,17 +19,18 @@ import spock.lang.Specification
 @ContextConfiguration("classpath:/com/griddynamics/cd/selenium/app-context.xml")
 class SeNodesSmokeTest extends Specification {
     @Value('${SELENIUM_URL}') String wdRootUrl
-    @Value('${SELENIUM_URL_TO_ACCESS_FROM_TESTS}') String wdRootUrlAccessibleFromTests
+    @Value('${HOST_WHERE_TESTS_RUNNING}') String testPageUrlToAccess
     @Value('${EXPECTED_NUMBER_OF_NODES}') int numberOfNodes
+    @Autowired Server server
     @Shared List<WebDriver> webDrivers
 
     def "one browser on every node should open a page of Se Grid Console"() {
         given: "$numberOfNodes sessions are established with Grid: $wdRootUrl"
             webDrivers = createWds(numberOfNodes)
-        when: "Accessing Grid Console from the test"
-            webDrivers.each { it.get("$wdRootUrlAccessibleFromTests/grid/console") }
-        then: "Some standard elements like nodes are visible on the grid console page"
-            webDrivers.every { !it.findElements(By.cssSelector(".content")).empty }
+        when: "Accessing test HTML page from the test: $testPageAddress"
+            webDrivers.each { it.get(testPageAddress) }
+        then: "Some basic elements like h1 are visible on the test HTML page"
+            webDrivers.every { !it.findElements(By.cssSelector("h1")).empty }
     }
 
     def cleanupSpec() {
@@ -36,6 +40,16 @@ class SeNodesSmokeTest extends Specification {
             } catch (any) {//do nothing
             }
         }
+    }
+
+    /**
+     * Combines hostname and port in order to build a URL to access test page. While hostname is set in the
+     * configuration or passed as a parameter, the port of the test page is dynamic and changes from run to run in
+     * order to protect from port conflicts.
+     * @return a built URL to access test page
+     */
+    private String getTestPageAddress() {
+        return "$testPageUrlToAccess:${((ServerConnector) server.connectors[0]).localPort}"
     }
 
     private List<WebDriver> createWds(int numberOfBrowsers) {
