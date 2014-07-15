4dcb8488a99ca0959db831e088f4ebb5dbb0797f
Nikolay Yurin <nyurin@griddynamics.com>
2014-07-15 18:24:13 +0400

    Added license headers for all source files
    Done by `mvn license:format`. Markdown files are to be fixed separately if
    any discrepancy found because they are not managed by license plugin.

--- selenium-hub-webclient/src/main/java/com/griddynamics/cd/selenium/entity/SeNodes.java
+++ selenium-hub-webclient/src/main/java/com/griddynamics/cd/selenium/entity/SeNodes.java
@@ -1,6 +1,7 @@
-/*
+/**
  * Copyright 2014, Grid Dynamics International, Inc.
  * Licensed under the Apache License, Version 2.0.
+ * Classification level: Public
  */
 package com.griddynamics.cd.selenium.entity;
 
